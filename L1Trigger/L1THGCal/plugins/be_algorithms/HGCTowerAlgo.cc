66092fc59bcb9d9bae982fc573b7a06d0c55f798
Gianluca <gianluca.cerminara@cern.ch>
2018-11-14 09:15:37 +0100

    allow configuration of product lables

--- L1Trigger/L1THGCal/plugins/be_algorithms/HGCTowerAlgo.cc
+++ L1Trigger/L1THGCal/plugins/be_algorithms/HGCTowerAlgo.cc
@@ -17,11 +17,11 @@ using namespace HGCalTriggerBackend;
 
 
 template<typename FECODEC, typename DATA>
-class HGCTowerAlgo : public Algorithm<FECODEC> 
+class HGCTowerAlgo : public Algorithm<FECODEC>
 {
     public:
         using Algorithm<FECODEC>::name;
-    
+
     protected:
         using Algorithm<FECODEC>::codec_;
 
@@ -35,19 +35,23 @@ class HGCTowerAlgo : public Algorithm<FECODEC>
         tower_product_( new l1t::HGCalTowerBxCollection ),
         calibration_( conf.getParameterSet("calib_parameters") ),
         towermap2D_( conf.getParameterSet("towermap_parameters") ),
-        towermap3D_( )
+        towermap3D_( ),
+        tcLabel_(conf.getParameter<std::string>("tcLabel")),
+        towerMapLabel_(conf.getParameter<std::string>("towerMapLabel")),
+        towerLabel_(conf.getParameter<std::string>("towerLabel"))
+
         {
         }
-	
-    
+
+
         void setProduces(edm::stream::EDProducer<>& prod) const final
         {
-            prod.produces<l1t::HGCalTriggerCellBxCollection>( "calibratedTriggerCellsTower" );            
-            prod.produces<l1t::HGCalTowerMapBxCollection>( "towerMap" );
-            prod.produces<l1t::HGCalTowerBxCollection>( "tower" );   
+            prod.produces<l1t::HGCalTriggerCellBxCollection>( tcLabel_ );
+            prod.produces<l1t::HGCalTowerMapBxCollection>( towerMapLabel_ );
+            prod.produces<l1t::HGCalTowerBxCollection>( towerLabel_ );
         }
-          
-            
+
+
         void run(const l1t::HGCFETriggerDigiCollection& coll, const edm::EventSetup& es, edm::Event&evt ) final;
 
 
@@ -59,80 +63,83 @@ class HGCTowerAlgo : public Algorithm<FECODEC>
 
         void reset() final
         {
-            trgcell_product_.reset( new l1t::HGCalTriggerCellBxCollection );    
+            trgcell_product_.reset( new l1t::HGCalTriggerCellBxCollection );
             towermap_product_.reset( new l1t::HGCalTowerMapBxCollection );
             tower_product_.reset( new l1t::HGCalTowerBxCollection );
         }
 
-    
+
     private:
 
         /* pointers to collections of trigger-cells, towerMaps and towers */
         std::unique_ptr<l1t::HGCalTriggerCellBxCollection> trgcell_product_;
         std::unique_ptr<l1t::HGCalTowerMapBxCollection> towermap_product_;
         std::unique_ptr<l1t::HGCalTowerBxCollection> tower_product_;
-    
+
         edm::ESHandle<HGCalTriggerGeometryBase> triggerGeometry_;
 
         /* algorithms instances */
         HGCalTriggerCellCalibration calibration_;
         HGCalTowerMap2DImpl towermap2D_;
         HGCalTowerMap3DImpl towermap3D_;
+        std::string tcLabel_;
+        std::string towerMapLabel_;
+        std::string towerLabel_;
 };
 
 
 template<typename FECODEC, typename DATA>
-void HGCTowerAlgo<FECODEC,DATA>::run(const l1t::HGCFETriggerDigiCollection & coll, 
+void HGCTowerAlgo<FECODEC,DATA>::run(const l1t::HGCFETriggerDigiCollection & coll,
                                        const edm::EventSetup & es,
-                                       edm::Event & evt ) 
+                                       edm::Event & evt )
 {
-    es.get<CaloGeometryRecord>().get("", triggerGeometry_);    
+    es.get<CaloGeometryRecord>().get("", triggerGeometry_);
     calibration_.eventSetup(es);
     towermap2D_.eventSetup(es);
 
     for( const auto& digi : coll ){
-        
+
         HGCalDetId module_id( digi.id() );
 
 
         DATA data;
         data.reset();
         digi.decode(codec_, data);
-        
+
         for(const auto& triggercell : data.payload)
         {
-            
+
             if( triggercell.hwPt() > 0 )
             {
                 l1t::HGCalTriggerCell calibratedtriggercell( triggercell );
                 calibration_.calibrateInGeV( calibratedtriggercell);
                 trgcell_product_->push_back( 0, calibratedtriggercell );
-            }           
-        
+            }
+
         }
-    
+
     }
 
     /* orphan handles to the collections of trigger-cells, towermaps and towers */
     edm::OrphanHandle<l1t::HGCalTriggerCellBxCollection> triggerCellsHandle;
     edm::OrphanHandle<l1t::HGCalTowerMapBxCollection> towerMapsHandle;
     edm::OrphanHandle<l1t::HGCalTowerBxCollection> towersHandle;
- 
+
     /* retrieve the orphan handle to the trigger-cells collection and put the collection in the event */
-    triggerCellsHandle = evt.put( std::move( trgcell_product_ ), "calibratedTriggerCellsTower");
+    triggerCellsHandle = evt.put( std::move( trgcell_product_ ), tcLabel_);
 
     /* create a persistent vector of pointers to the trigger-cells */
     std::vector<edm::Ptr<l1t::HGCalTriggerCell>> triggerCellsPtrs;
     for( unsigned i = 0; i < triggerCellsHandle->size(); ++i ) {
         edm::Ptr<l1t::HGCalTriggerCell> ptr(triggerCellsHandle,i);
         triggerCellsPtrs.push_back(ptr);
-    }    
+    }
 
     /* call to towerMap2D clustering */
     towermap2D_.buildTowerMap2D( triggerCellsPtrs, *towermap_product_);
 
     /* retrieve the orphan handle to the towermaps collection and put the collection in the event */
-    towerMapsHandle = evt.put( std::move( towermap_product_ ), "towerMap");
+    towerMapsHandle = evt.put( std::move( towermap_product_ ), towerMapLabel_);
 
     /* create a persistent vector of pointers to the towerMaps */
     std::vector<edm::Ptr<l1t::HGCalTowerMap>> towerMapsPtrs;
@@ -145,7 +152,7 @@ void HGCTowerAlgo<FECODEC,DATA>::run(const l1t::HGCFETriggerDigiCollection & col
     towermap3D_.buildTowerMap3D( towerMapsPtrs, *tower_product_);
 
     /* retrieve the orphan handle to the tower collection and put the collection in the event */
-    towersHandle = evt.put( std::move( tower_product_ ), "tower");
+    towersHandle = evt.put( std::move( tower_product_ ), towerLabel_);
 
 }
 
@@ -153,10 +160,10 @@ typedef HGCTowerAlgo<HGCalTriggerCellBestChoiceCodec, HGCalTriggerCellBestChoice
 typedef HGCTowerAlgo<HGCalTriggerCellThresholdCodec, HGCalTriggerCellThresholdCodec::data_type> HGCTowerAlgoThreshold;
 
 
-DEFINE_EDM_PLUGIN(HGCalTriggerBackendAlgorithmFactory, 
+DEFINE_EDM_PLUGIN(HGCalTriggerBackendAlgorithmFactory,
         HGCTowerAlgoBestChoice,
         "HGCTowerAlgoBestChoice");
 
-DEFINE_EDM_PLUGIN(HGCalTriggerBackendAlgorithmFactory, 
+DEFINE_EDM_PLUGIN(HGCalTriggerBackendAlgorithmFactory,
         HGCTowerAlgoThreshold,
         "HGCTowerAlgoThreshold");
