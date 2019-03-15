9ca1e31328241220c68930852a2fa4c7c3666dd6
Gianluca <gianluca.cerminara@cern.ch>
2019-03-15 09:55:53 +0100

    revert changes implemented during conflict merge

--- L1Trigger/L1THGCalUtilities/python/clustering3d.py
+++ L1Trigger/L1THGCalUtilities/python/clustering3d.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from L1Trigger.L1THGCal.customClustering import binSums, dr_layerbylayer
 
+
 def create_distance(process, inputs,
         distance=0.01
         ):
@@ -19,6 +20,7 @@ def create_dbscan(process, inputs,
         min_points=3
         ):
     producer = process.hgcalBackEndLayer2Producer.clone() 
+    producer.ProcessorParameters.C3d_parameters.dR_multicluster = cms.double(0.)
     producer.ProcessorParameters.C3d_parameters.dist_dbscan_multicluster = cms.double(distance)
     producer.ProcessorParameters.C3d_parameters.minN_dbscan_multicluster = cms.uint32(min_points)
     producer.ProcessorParameters.C3d_parameters.type_multicluster = cms.string('DBSCANC3d')
