bd2bcfc60043af38a09c31b4da3177828efb70da
Gianluca <gianluca.cerminara@cern.ch>
2019-04-10 15:07:35 +0200

    add customization of the FE and clustering thresholds to adapt to calibrations used in V9

--- L1Trigger/L1THGCal/python/hgcalBackEndLayer2Producer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalBackEndLayer2Producer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 import SimCalorimetry.HGCalSimProducers.hgcalDigitizer_cfi as digiparam
 import RecoLocalCalo.HGCalRecProducers.HGCalUncalibRecHit_cfi as recoparam
-import RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi as recocalibparam 
+import RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi as recocalibparam
 
 from L1Trigger.L1THGCal.egammaIdentification import egamma_identification_histomax
 from L1Trigger.L1THGCal.customClustering import binSums, dr_layerbylayer
@@ -23,6 +23,17 @@ C3d_parValues = cms.PSet( type_multicluster = cms.string('HistoMaxC3d'),
                                                           0    , 0.25, 0
                                                           )
  )
+
+
+from Configuration.Eras.Modifier_phase2_hgcalV9_cff import phase2_hgcalV9
+# V9 samples have a different defintiion of the dEdx calibrations. To account for it
+# we reascale the thresholds of the clustering seeds 
+# (see https://indico.cern.ch/event/806845/contributions/3359859/attachments/1815187/2966402/19-03-20_EGPerf_HGCBE.pdf
+# for more details)
+phase2_hgcalV9.toModify(C3d_parValues,
+                        threshold_histo_multicluster=cms.double(7.5),  # MipT
+                        )
+
 
 be_proc = cms.PSet( ProcessorName  = cms.string('HGCalBackendLayer2Processor3DClustering'),
                     C3d_parameters = C3d_parValues.clone()
--- L1Trigger/L1THGCal/python/hgcalConcentratorProducer_cfi.py
+++ L1Trigger/L1THGCal/python/hgcalConcentratorProducer_cfi.py
@@ -18,6 +18,17 @@ conc_proc = cms.PSet(ProcessorName  = cms.string('HGCalConcentratorProcessorSele
                      triggercell_threshold_scintillator = cms.double(2.), # MipT
                      )
 
+from Configuration.Eras.Modifier_phase2_hgcalV9_cff import phase2_hgcalV9
+# V9 samples have a different defintiion of the dEdx calibrations. To account for it
+# we reascale the thresholds of the FE selection
+# (see https://indico.cern.ch/event/806845/contributions/3359859/attachments/1815187/2966402/19-03-20_EGPerf_HGCBE.pdf
+# for more details)
+phase2_hgcalV9.toModify(conc_proc,
+                        triggercell_threshold_silicon=cms.double(1.5),  # MipT
+                        triggercell_threshold_scintillator=cms.double(1.5),  # MipT
+                        )
+
+
 hgcalConcentratorProducer = cms.EDProducer(
     "HGCalConcentratorProducer",
     InputTriggerCells = cms.InputTag('hgcalVFEProducer:HGCalVFEProcessorSums'),
