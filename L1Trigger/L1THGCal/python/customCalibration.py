9f6beb9cc5794237662a613b341e29875cc01134
Gianluca <gianluca.cerminara@cern.ch>
2019-04-10 12:27:51 +0200

    add customization of the FE and clustering thresholds to adapt to calibrations used in V9

--- L1Trigger/L1THGCal/python/customCalibration.py
+++ L1Trigger/L1THGCal/python/customCalibration.py
@@ -7,7 +7,7 @@ def custom_cluster_calibration_global(process,
         factor=1.084
         ):
     parameters_c2d = process.hgcalBackEndLayer1Producer.ProcessorParameters.C2d_parameters
-    parameters_c2d.calibSF_cluster = cms.double(factor) 
+    parameters_c2d.calibSF_cluster = cms.double(factor)
     parameters_c2d.applyLayerCalibration = cms.bool(False)
     return process
 
@@ -18,4 +18,11 @@ def custom_cluster_calibration_layers(process,
     parameters_c2d = process.hgcalBackEndLayer1Producer.ProcessorParameters.C2d_parameters
     parameters_c2d.layerWeights = weights
     parameters_c2d.applyLayerCalibration = cms.bool(True)
+    return process
+
+
+def custom_fe_thresholds(process, fe_threshold=1.5, cl3d_seed_threshold=7.5):
+    process.hgcalConcentratorProducer.ProcessorParameters.triggercell_threshold_scintillator = cms.double(fe_threshold)
+    process.hgcalConcentratorProducer.ProcessorParameters.triggercell_threshold_silicon = cms.double(fe_threshold)
+    process.hgcalBackEndLayer2Producer.ProcessorParameters.C3d_parameters.threshold_histo_multicluster = cl3d_seed_threshold
     return process
--- L1Trigger/L1THGCal/python/hgcalTriggerPrimitives_cff.py
+++ L1Trigger/L1THGCal/python/hgcalTriggerPrimitives_cff.py
@@ -13,9 +13,10 @@ hgcalTriggerPrimitives = cms.Sequence(hgcalVFE*hgcalConcentrator*hgcalBackEndLay
 
 from Configuration.Eras.Modifier_phase2_hgcalV9_cff import phase2_hgcalV9
 from L1Trigger.L1THGCal.customTriggerGeometry import custom_geometry_V9
-from L1Trigger.L1THGCal.customCalibration import  custom_cluster_calibration_global
+from L1Trigger.L1THGCal.customCalibration import  custom_cluster_calibration_global, custom_fe_thresholds
 modifyHgcalTriggerPrimitivesWithV9Geometry_ = phase2_hgcalV9.makeProcessModifier(custom_geometry_V9)
 modifyHgcalTriggerPrimitivesCalibWithV9Geometry_ = phase2_hgcalV9.makeProcessModifier(lambda process : custom_cluster_calibration_global(process, factor=1))
+modifyHgcalTriggerPrimitivesCalibThresholdsWithV9Geometry_ = phase2_hgcalV9.makeProcessModifier(lambda process : custom_fe_thresholds(process, fe_threshold=1.5, cl3d_seed_threshold=7.5))
 
 from Configuration.ProcessModifiers.convertHGCalDigisSim_cff import convertHGCalDigisSim
 # can't declare a producer version of simHGCalUnsuppressedDigis in the normal flow of things,
